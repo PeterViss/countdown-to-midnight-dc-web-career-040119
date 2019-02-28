d1304f5c6291af82db60ab34f56f2ad9555dd893
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-28 02:53:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 #write your code here
 def countdown(n)
-n = 10
+
   while n > 0
     puts "#{n} SECOND(S)!"
     n -= 1
@@ -9,7 +9,7 @@ n = 10
 end
 
 def countdown_with_sleep(n)
-n = 5
+
   while n > 0
     puts "#{n} SECOND(S!)"
     sleep(1)
